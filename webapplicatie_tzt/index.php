535215e42a52bbad4ba36c4ab71b0f5bf6f737ee
THUIS <THUIS@145.44.65.210>
2015-04-14 14:36:25 +0200

    Merge origin/master
    Conflicts:
    	SysteemAdmin/AdminTicketOverzicht.php
    	SysteemAdmin/AdminTicketSelecteren.php

--- webapplicatie_tzt/index.php
+++ webapplicatie_tzt/index.php
@@ -26,6 +26,42 @@ and open the template in the editor.
                     </ul>
                 </div>
             </header>
+            <div id="slider">
+                <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                    <!-- Indicators -->
+                    <ol class="carousel-indicators">
+                      <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                      <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                      <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                      <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+                    </ol>
+
+                    <!-- Wrapper for slides -->
+                    <div class="carousel-inner" role="listbox">
+                      <div class="item active">
+                        <img src="image/slide1.jpg" alt="...">
+                        <div class="carousel-caption">
+                          Test bericht 1
+                        </div>
+                      </div>
+                      <div class="item">
+                        <img src="image/slide2.jpg" alt="...">
+                        <div class="carousel-caption">
+                          Test bericht 2
+                        </div>
+                      </div>
+                      <div class="item">
+                        <img src="image/slide3.jpg" alt="...">
+                        <div class="carousel-caption">
+                          Test bericht 3
+                      </div>
+                      </div>
+                    </div>
+                </div>
+            </div>
+            <div id="content">
+                
+            </div>
             <footer>
 
             </footer>
