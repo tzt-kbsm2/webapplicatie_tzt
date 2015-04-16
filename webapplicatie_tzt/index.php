d5a42e5b54405d5c128d2652b032dfadc6ee5666
THUIS <THUIS@145.44.64.211>
2015-04-16 11:31:45 +0200

    Merge origin/master
    Conflicts:
    	SysteemAdmin/AdminTicketOverzicht.php
    	SysteemAdmin/AdminTicketSelecteren.php

--- webapplicatie_tzt/index.php
+++ webapplicatie_tzt/index.php
@@ -14,85 +14,85 @@ and open the template in the editor.
         <title>Tzt WebApplicatie</title>
     </head>
     <body>
-        <div class="container">
             <header>
-                
-                <div class="logo">
-                    <img src="image/logoTzt.jpg" alt="" width="" height="80"/>
-                </div>
-                <div id="menu">
-                    <ul class="nav nav-pills">
-                        <li role="presentation" class="active"><a href="#">Home</a></li>
-                        <li role="presentation"><a href="#">Over</a></li>
-                        <li role="presentation"><a href="#">Online versturen</a></li>
-                        <li role="presentation"><a href="contactPagina.php">Contact</a></li>
-                        <li>
-                            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#loginModal" data-whatever="Inloggen">Login</button>
+                <div class="container">
+                    <div class="logo">
+                        <img src="image/logoTzt.jpg" alt="" width="" height="80"/>
+                    </div>
+                    <div id="menu">
+                        <ul class="nav nav-pills">
+                            <li role="presentation" class="active"><a href="#">Home</a></li>
+                            <li role="presentation"><a href="#">Over</a></li>
+                            <li role="presentation"><a href="#">Online versturen</a></li>
+                            <li role="presentation"><a href="contactPagina.php">Contact</a></li>
+                            <li>
+                                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#loginModal" data-whatever="Inloggen">Login</button>
 
-                            <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="loginModalLabel" aria-hidden="true">
-                              <div class="modal-dialog">
-                                <div class="modal-content">
-                                  <div class="modal-header">
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                                    <h4 class="modal-title" id="exampleModalLabel">Inloggen</h4>
-                                  </div>
-                                  <div class="modal-body">
-                                    <form>
-                                      <div class="form-group">
-                                        <label for="recipient-name" class="control-label">Gebruikersnaam:</label>
-                                        <input type="text" class="form-control" id="recipient-name">
+                                <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="loginModalLabel" aria-hidden="true">
+                                  <div class="modal-dialog">
+                                    <div class="modal-content">
+                                      <div class="modal-header">
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                        <h4 class="modal-title" id="exampleModalLabel">Inloggen</h4>
                                       </div>
-                                      <div class="form-group">
-                                        <label for="recipient-ww" class="control-label">Wachtwoord:</label>
-                                        <input type="password" class="form-control" id="recipient-ww">
+                                      <div class="modal-body">
+                                        <form>
+                                          <div class="form-group">
+                                            <label for="recipient-name" class="control-label">Gebruikersnaam:</label>
+                                            <input type="text" class="form-control" id="recipient-name">
+                                          </div>
+                                          <div class="form-group">
+                                            <label for="recipient-ww" class="control-label">Wachtwoord:</label>
+                                            <input type="password" class="form-control" id="recipient-ww">
+                                          </div>
+                                        </form>
                                       </div>
-                                    </form>
-                                  </div>
-                                  <div class="modal-footer">
-                                    <button type="button" class="btn btn-default" data-dismiss="modal">Terug</button>
-                                    <button type="button" class="btn btn-primary">Inloggen</button>
+                                      <div class="modal-footer">
+                                        <button type="button" class="btn btn-default" data-dismiss="modal">Terug</button>
+                                        <button type="button" class="btn btn-primary">Inloggen</button>
+                                      </div>
+                                    </div>
                                   </div>
                                 </div>
-                              </div>
-                            </div>
-                        </li>
-                    </ul>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-                
-   
             </header>
-            <div id="slider">
-                <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-                    <!-- Indicators -->
-                    <ol class="carousel-indicators">
-                      <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                      <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                      <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-                    </ol>
+            <div class="container">
+                <div id="slider">
+                    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                        <!-- Indicators -->
+                        <ol class="carousel-indicators">
+                          <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                          <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                          <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                        </ol>
 
-                    <!-- Wrapper for slides -->
-                    <div class="carousel-inner" role="listbox">
-                      <div class="item active">
-                        <img src="image/slide1.jpg" alt="...">
-                        <div class="carousel-caption">
-                          Test bericht 1
+                        <!-- Wrapper for slides -->
+                        <div class="carousel-inner" role="listbox">
+                          <div class="item active">
+                            <img src="image/slide1.jpg" alt="...">
+                            <div class="carousel-caption">
+                              Test bericht 1
+                            </div>
+                          </div>
+
+                          <div class="item">
+                            <img src="image/slide2.jpg" alt="...">
+                            <div class="carousel-caption">
+                              Test bericht 2
+                            </div>
+                          </div>
+
+                          <div class="item">
+                            <img src="image/slide3.jpg" alt="...">
+                            <div class="carousel-caption">
+                              Test bericht 3
+                          </div>
+                          </div>
+
                         </div>
-                      </div>
-                        
-                      <div class="item">
-                        <img src="image/slide2.jpg" alt="...">
-                        <div class="carousel-caption">
-                          Test bericht 2
-                        </div>
-                      </div>
-                        
-                      <div class="item">
-                        <img src="image/slide3.jpg" alt="...">
-                        <div class="carousel-caption">
-                          Test bericht 3
-                      </div>
-                      </div>
-                        
                     </div>
                 </div>
             </div>
@@ -116,6 +116,7 @@ and open the template in the editor.
                         </div>
                     </div>
                 </div>
+                <div class="container">
                 <div class="row">
                     <div class="col-sm-6 col-md-4">
                       <div class="thumbnail">
@@ -152,7 +153,6 @@ and open the template in the editor.
                     </div>
                 </div>
             </div>
-        </div>
             <footer>
                 <div class="container">
                     <div class="row">
