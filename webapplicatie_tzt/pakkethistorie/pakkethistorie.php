85b4674291755fbfaa655b0bb84f3d96b11c28cd
THUIS <THUIS@145.44.64.211>
2015-04-16 13:56:55 +0200

    Merge origin/master
    Conflicts:
    	SysteemAdmin/AdminTicketOverzicht.php
    	SysteemAdmin/AdminTicketSelecteren.php

--- webapplicatie_tzt/pakkethistorie/pakkethistorie.php
+++ webapplicatie_tzt/pakkethistorie/pakkethistorie.php
@@ -1,8 +1,30 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+                include "database.php";
+                $username = $_SESSION['Email'];
+                $password = $_SESSION['Password'];
 
+                $loginid = mysqli_prepare($database, "SELECT Email FROM SendCustomer WHERE Email='$username'");
+                mysqli_stmt_execute($loginid);
+                mysqli_stmt_bind_result($loginid, $email);
+                mysqli_stmt_fetch($loginid, $email);
+                
+                $stmt1 = mysqli_prepare($database, "SELECT PackageID, SendDate, DeliverDate, StartLocation, EndLocation, CoerierType FROM Package P JOIN SendCustomer S ON P.PackageID = S.CustomerID WHERE S.CustomerID = $email ");
+                mysqli_stmt_execute($stmt1);
+                mysqli_stmt_bind_result($stmt1, $package, $senddate, $deliverdate, $startlocation, $endlocation, $coeriertype);
+                while (mysqli_stmt_fetch($stmt1))
+                {
+                    echo '<td>'.$package.'</td>';
+                    echo '<td>'.$senddate.'</td>';
+                    echo '<td>'.$deliverdate.'</td>';
+                    echo '<td>'.$startlocation.'</td>';
+                    echo '<td>'.$endlocation.'</td>';
+                    echo '<td>'.$coeriertype.'</td>';
+                }
+
+                mysqli_close($database);
+                
+                
+
+
+?>
