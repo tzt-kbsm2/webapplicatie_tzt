ddb12074fa8ae46f62bbcf9ddb13e75d9bc1e311
THUIS <THUIS@145.44.64.211>
2015-04-16 13:59:46 +0200

    Merge origin/master
    Conflicts:
    	SysteemAdmin/AdminTicketOverzicht.php
    	SysteemAdmin/AdminTicketSelecteren.php

--- webapplicatie_tzt/pakkethistorie/pakkethistorie.php
+++ webapplicatie_tzt/pakkethistorie/pakkethistorie.php
@@ -4,12 +4,12 @@
                 $username = $_SESSION['Email'];
                 $password = $_SESSION['Password'];
 
-                $loginid = mysqli_prepare($database, "SELECT Email FROM SendCustomer WHERE Email='$username'");
+                $loginid = mysqli_prepare($database, "SELECT Email, CustomerID FROM SendCustomer WHERE Email='$username'");
                 mysqli_stmt_execute($loginid);
-                mysqli_stmt_bind_result($loginid, $email);
-                mysqli_stmt_fetch($loginid, $email);
+                mysqli_stmt_bind_result($loginid, $email, $customerid);
+                mysqli_stmt_fetch($loginid, $email, $customerid);
                 
-                $stmt1 = mysqli_prepare($database, "SELECT PackageID, SendDate, DeliverDate, StartLocation, EndLocation, CoerierType FROM Package P JOIN SendCustomer S ON P.PackageID = S.CustomerID WHERE S.CustomerID = $email ");
+                $stmt1 = mysqli_prepare($database, "SELECT PackageID, SendDate, DeliverDate, StartLocation, EndLocation, CoerierType FROM Package P JOIN SendCustomer S ON P.PackageID = S.CustomerID WHERE S.CustomerID = $customerid ");
                 mysqli_stmt_execute($stmt1);
                 mysqli_stmt_bind_result($stmt1, $package, $senddate, $deliverdate, $startlocation, $endlocation, $coeriertype);
                 while (mysqli_stmt_fetch($stmt1))
@@ -23,8 +23,5 @@
                 }
 
                 mysqli_close($database);
-                
-                
-
 
 ?>
